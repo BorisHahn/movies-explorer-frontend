5e86041f164536d71873e2023918f1b555f21ea3
Boris Gaidukevich <Borishahn@yandex.ru>
2022-12-13 03:09:52 +0300

    0.1.0

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -4,7 +4,7 @@ const SearchForm = () => {
   return (
     <section className='search-form'>
       <div className='search-form__wrapper'>
-        <input className='search-form__input' placeholder='Фильм'></input>
+        <input className='search-form__input' placeholder='Фильм' required></input>
         <button className='search-form__button'>Найти</button>
       </div>
       <div className='search-form__toggle'>
